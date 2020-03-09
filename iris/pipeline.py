83c3d69c80d91c14c30ff637e6823182cc99fde1
Valeriano Manassero <valeriano.manassero@namecheap.com>
2020-03-09 13:15:42 +0100

    deprecation updates

--- iris/pipeline.py
+++ iris/pipeline.py
@@ -1,5 +1,6 @@
 import os
 from typing import Text
+import tensorflow_model_analysis as tfma
 from tfx.components.evaluator.component import Evaluator
 from tfx.components.example_gen.csv_example_gen.component import CsvExampleGen
 from tfx.components.example_validator.component import ExampleValidator
@@ -10,9 +11,7 @@ from tfx.components.statistics_gen.component import StatisticsGen
 from tfx.components.trainer.component import Trainer
 from tfx.components.transform.component import Transform
 from tfx.orchestration import pipeline
-from tfx.orchestration.kubeflow.runner import KubeflowRunner
-from tfx.orchestration.kubeflow.kubeflow_dag_runner import KubeflowDagRunnerConfig
-from tfx.proto import evaluator_pb2
+from tfx.orchestration.kubeflow.runner import kubeflow_dag_runner
 from tfx.proto import example_gen_pb2
 from tfx.proto import pusher_pb2
 from tfx.proto import trainer_pb2
@@ -21,76 +20,98 @@ from kfp import onprem
 
 
 _pipeline_name = 'iris'
-_tfx_root = '/mnt/tfx-pv'
+
+_persistent_volume_claim = 'tfx-pvc'
+_persistent_volume = 'tfx-pv'
+_persistent_volume_mount = '/mnt'
+
+_input_base = os.path.join(_persistent_volume_mount, 'iris')
+_output_base = os.path.join(_persistent_volume_mount, 'pipelines')
+_tfx_root = os.path.join(_output_base, 'tfx')
 _pipeline_root = os.path.join(_tfx_root, _pipeline_name)
-_module_file = os.path.join(_pipeline_root, 'utils.py')
-_serving_model_dir = os.path.join(_pipeline_root, 'serving_model', _pipeline_name)
+
+_data_root = os.path.join(_input_base, 'data')
+
+_module_file = os.path.join(_input_base, 'utils.py')
+
+_serving_model_dir = os.path.join(_output_base, _pipeline_name, 'serving_model')
 
 
-def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
-                     module_file: Text, serving_model_dir: Text) -> pipeline.Pipeline:
+def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text, module_file: Text,
+                     serving_model_dir: Text, direct_num_workers: int) -> pipeline.Pipeline:
     examples = external_input(data_root)
     input_split = example_gen_pb2.Input(splits=[
         example_gen_pb2.Input.Split(name='train', pattern='iris_training.csv'),
         example_gen_pb2.Input.Split(name='eval', pattern='iris_test.csv')
     ])
-    example_gen = CsvExampleGen(input_base=examples, input_config=input_split)
-    statistics_gen = StatisticsGen(input_data=example_gen.outputs.examples)
-    infer_schema = SchemaGen(stats=statistics_gen.outputs.output)
+    example_gen = CsvExampleGen(input=examples, input_config=input_split)
+    statistics_gen = StatisticsGen(examples=example_gen.outputs['examples'])
+    infer_schema = SchemaGen(statistics=statistics_gen.outputs['statistics'])
     validate_stats = ExampleValidator(
-        stats=statistics_gen.outputs.output,
-        schema=infer_schema.outputs.output)
+        statistics=statistics_gen.outputs['statistics'],
+        schema=infer_schema.outputs['schema'])
     transform = Transform(
-        input_data=example_gen.outputs.examples,
-        schema=infer_schema.outputs.output,
+        examples=example_gen.outputs['examples'],
+        schema=infer_schema.outputs['schema'],
         module_file=module_file)
-    trainer = Trainer(
-        module_file=module_file,
-        examples=transform.outputs.transformed_examples,
-        schema=infer_schema.outputs.output,
-        transform_output=transform.outputs.transform_output,
-        train_args=trainer_pb2.TrainArgs(num_steps=1000),
-        eval_args=trainer_pb2.EvalArgs(num_steps=500))
+    trainer = Trainer(module_file=module_file,
+                      transformed_examples=transform.outputs['transformed_examples'],
+                      schema=infer_schema.outputs['schema'],
+                      transform_graph=transform.outputs['transform_graph'],
+                      train_args=trainer_pb2.TrainArgs(num_steps=1000),
+                      eval_args=trainer_pb2.EvalArgs(num_steps=500)
+                      )
+    eval_config = tfma.EvalConfig(
+        slicing_specs=[tfma.SlicingSpec()]
+    )
     model_analyzer = Evaluator(
-        examples=example_gen.outputs.examples,
-        model_exports=trainer.outputs.output,
-        feature_slicing_spec=evaluator_pb2.FeatureSlicingSpec(specs=[
-            evaluator_pb2.SingleSlicingSpec()
-        ]))
+        examples=example_gen.outputs['examples'],
+        model=trainer.outputs['model'],
+        eval_config=eval_config
+    )
     model_validator = ModelValidator(
-        examples=example_gen.outputs.examples, model=trainer.outputs.output)
-    pusher = Pusher(
-        model_export=trainer.outputs.output,
-        model_blessing=model_validator.outputs.blessing,
-        push_destination=pusher_pb2.PushDestination(
-            filesystem=pusher_pb2.PushDestination.Filesystem(
-                base_directory=serving_model_dir)))
+        examples=example_gen.outputs['examples'],
+        model=trainer.outputs['model'])
+
+    pusher = Pusher(model=trainer.outputs['model'],
+                    model_blessing=model_validator.outputs['blessing'],
+                    push_destination=pusher_pb2.PushDestination(
+                        filesystem=pusher_pb2.PushDestination.Filesystem(base_directory=serving_model_dir)
+                    ))
 
     return pipeline.Pipeline(
         pipeline_name=pipeline_name,
         pipeline_root=pipeline_root,
-        components=[
-            example_gen, statistics_gen, infer_schema, validate_stats, transform,
-            trainer, model_analyzer, model_validator, pusher
-        ],
-        log_root='/var/tmp/tfx/logs',
+        components=[example_gen, statistics_gen, infer_schema, validate_stats, transform, trainer, model_analyzer,
+                    model_validator, pusher],
+        beam_pipeline_args=['--direct_num_workers=%d' % direct_num_workers]
     )
-
 
 if __name__ == '__main__':
-    mount_volume_op = onprem.mount_pvc(
-        "tfx-pvc",
-        "tfx-pv",
-        _tfx_root)
-    config = KubeflowDagRunnerConfig(
-        pipeline_operator_funcs=[mount_volume_op],
-        tfx_image='tensorflow/tfx:0.14.0'
-    )
-    _pipeline = _create_pipeline(
+    tfx_image = os.environ.get('KUBEFLOW_TFX_IMAGE', None)
+
+    metadata_config = kubeflow_dag_runner.get_default_kubeflow_metadata_config()
+    metadata_config.mysql_db_service_host.value = 'mysql.kubeflow'
+    metadata_config.mysql_db_service_port.value = "3306"
+    metadata_config.mysql_db_name.value = "metadb"
+    metadata_config.mysql_db_user.value = "root"
+    metadata_config.mysql_db_password.value = ""
+    metadata_config.grpc_config.grpc_service_host.value = 'metadata-grpc-service'
+    metadata_config.grpc_config.grpc_service_port.value = '8080'
+
+    runner_config = kubeflow_dag_runner.KubeflowDagRunnerConfig(tfx_image=tfx_image,
+                                                                pipeline_operator_funcs=([
+                                                                    onprem.mount_pvc(_persistent_volume_claim,
+                                                                                     _persistent_volume,
+                                                                                     _persistent_volume_mount)
+                                                                ]),
+                                                                kubeflow_metadata_config=metadata_config
+                                                                )
+
+    kubeflow_dag_runner.KubeflowDagRunner(config=runner_config).run(_create_pipeline(
         pipeline_name=_pipeline_name,
         pipeline_root=_pipeline_root,
-        data_root=os.path.join(_pipeline_root, 'data'),
+        data_root=_data_root,
         module_file=_module_file,
         serving_model_dir=_serving_model_dir,
-        )
-    KubeflowRunner(config=config).run(_pipeline)
+        direct_num_workers=0))

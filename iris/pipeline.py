693b3fbb6f04626c3446a4decba5a5401c55d3e1
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2020-03-11 18:49:24 +0100

    Update pipelines (#19)
    * pipeline updates for Keras usage
    
    * data anlysis using metadata prototype
    
    * data analysis reference
    
    * custom image reference

--- iris/pipeline.py
+++ iris/pipeline.py
@@ -1,20 +1,25 @@
 import os
 from typing import Text
 import tensorflow_model_analysis as tfma
+from tfx.components import ResolverNode
 from tfx.components.evaluator.component import Evaluator
+from tfx.components.base import executor_spec
 from tfx.components.example_gen.csv_example_gen.component import CsvExampleGen
 from tfx.components.example_validator.component import ExampleValidator
-from tfx.components.model_validator.component import ModelValidator
 from tfx.components.pusher.component import Pusher
 from tfx.components.schema_gen.component import SchemaGen
 from tfx.components.statistics_gen.component import StatisticsGen
 from tfx.components.trainer.component import Trainer
+from tfx.components.trainer.executor import GenericExecutor
 from tfx.components.transform.component import Transform
+from tfx.dsl.experimental import latest_blessed_model_resolver
 from tfx.orchestration import pipeline
 from tfx.orchestration.kubeflow.runner import kubeflow_dag_runner
-from tfx.proto import example_gen_pb2
 from tfx.proto import pusher_pb2
 from tfx.proto import trainer_pb2
+from tfx.types import Channel
+from tfx.types.standard_artifacts import Model
+from tfx.types.standard_artifacts import ModelBlessing
 from tfx.utils.dsl_utils import external_input
 from kfp import onprem
 
@@ -40,13 +45,9 @@ _serving_model_dir = os.path.join(_output_base, _pipeline_name, 'serving_model')
 def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text, module_file: Text,
                      serving_model_dir: Text, direct_num_workers: int) -> pipeline.Pipeline:
     examples = external_input(data_root)
-    input_split = example_gen_pb2.Input(splits=[
-        example_gen_pb2.Input.Split(name='train', pattern='iris_training.csv'),
-        example_gen_pb2.Input.Split(name='eval', pattern='iris_test.csv')
-    ])
-    example_gen = CsvExampleGen(input=examples, input_config=input_split)
+    example_gen = CsvExampleGen(input=examples)
     statistics_gen = StatisticsGen(examples=example_gen.outputs['examples'])
-    infer_schema = SchemaGen(statistics=statistics_gen.outputs['statistics'])
+    infer_schema = SchemaGen(statistics=statistics_gen.outputs['statistics'], infer_feature_shape=True)
     validate_stats = ExampleValidator(
         statistics=statistics_gen.outputs['statistics'],
         schema=infer_schema.outputs['schema'])
@@ -55,26 +56,35 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text, 
         schema=infer_schema.outputs['schema'],
         module_file=module_file)
     trainer = Trainer(module_file=module_file,
+                      custom_executor_spec=executor_spec.ExecutorClassSpec(GenericExecutor),
                       transformed_examples=transform.outputs['transformed_examples'],
                       schema=infer_schema.outputs['schema'],
                       transform_graph=transform.outputs['transform_graph'],
                       train_args=trainer_pb2.TrainArgs(num_steps=1000),
                       eval_args=trainer_pb2.EvalArgs(num_steps=500)
                       )
-    eval_config = tfma.EvalConfig(
-        slicing_specs=[tfma.SlicingSpec()]
-    )
+    model_resolver = ResolverNode(instance_name='latest_blessed_model_resolver',
+                                  resolver_class=latest_blessed_model_resolver.LatestBlessedModelResolver,
+                                  model=Channel(type=Model),
+                                  model_blessing=Channel(type=ModelBlessing))
+    eval_config = tfma.EvalConfig(model_specs=[tfma.ModelSpec(label_key='species')],
+                                  slicing_specs=[tfma.SlicingSpec()],
+                                  metrics_specs=[tfma.MetricsSpec(
+                                      thresholds={'sparse_categorical_accuracy': tfma.config.MetricThreshold(
+                                          value_threshold=tfma.GenericValueThreshold(
+                                              lower_bound={'value': 0.9}),
+                                          change_threshold=tfma.GenericChangeThreshold(
+                                              direction=tfma.MetricDirection.HIGHER_IS_BETTER,
+                                              absolute={'value': -1e-10}))
+                                      })
+                                  ])
     model_analyzer = Evaluator(
         examples=example_gen.outputs['examples'],
         model=trainer.outputs['model'],
-        eval_config=eval_config
-    )
-    model_validator = ModelValidator(
-        examples=example_gen.outputs['examples'],
-        model=trainer.outputs['model'])
-
+        baseline_model=model_resolver.outputs['model'],
+        eval_config=eval_config)
     pusher = Pusher(model=trainer.outputs['model'],
-                    model_blessing=model_validator.outputs['blessing'],
+                    model_blessing=model_analyzer.outputs['blessing'],
                     push_destination=pusher_pb2.PushDestination(
                         filesystem=pusher_pb2.PushDestination.Filesystem(base_directory=serving_model_dir)
                     ))
@@ -82,13 +92,14 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text, 
     return pipeline.Pipeline(
         pipeline_name=pipeline_name,
         pipeline_root=pipeline_root,
-        components=[example_gen, statistics_gen, infer_schema, validate_stats, transform, trainer, model_analyzer,
-                    model_validator, pusher],
-        beam_pipeline_args=['--direct_num_workers=%d' % direct_num_workers]
+        components=[example_gen, statistics_gen, infer_schema, validate_stats, transform, trainer, model_resolver,
+                    model_analyzer, pusher],
+        beam_pipeline_args=['--direct_num_workers=%d' % direct_num_workers],
+        enable_cache=True
     )
 
 if __name__ == '__main__':
-    tfx_image = os.environ.get('KUBEFLOW_TFX_IMAGE', None)
+    tfx_image = 'valerianomanassero/tfx-nvidia-gpu:1.0.1'
 
     metadata_config = kubeflow_dag_runner.get_default_kubeflow_metadata_config()
     metadata_config.mysql_db_service_host.value = 'mysql.kubeflow'
--- iris/utils.py
+++ iris/utils.py
@@ -1,9 +1,14 @@
-import tensorflow as tf
-import tensorflow_model_analysis as tfma
-import tensorflow_transform as tft
-from tensorflow_transform.tf_metadata import schema_utils
+from typing import Text
 
-_CATEGORICAL_FEATURE_KEYS = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+import absl
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_transform as tft
+
+from tfx.components.trainer.executor import TrainerFnArgs
+
+
+_FEATURE_KEYS = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
 _LABEL_KEY = 'species'
 
 
@@ -11,132 +16,102 @@ def _transformed_name(key):
     return key + '_xf'
 
 
-def _transformed_names(keys):
-    return [_transformed_name(key) for key in keys]
-
-
 def _gzip_reader_fn(filenames):
-    return tf.data.TFRecordDataset(
-        filenames,
-        compression_type='GZIP')
+    return tf.data.TFRecordDataset(filenames, compression_type='GZIP')
 
 
-def _fill_in_missing(x):
-    default_value = '' if x.dtype == tf.string else 0
-    return tf.squeeze(
-        tf.sparse.to_dense(
-            tf.SparseTensor(x.indices, x.values, [x.dense_shape[0], 1]),
-            default_value),
-        axis=1)
+def _get_serve_tf_examples_fn(model, tf_transform_output):
+    model.tft_layer = tf_transform_output.transform_features_layer()
+
+    @tf.function
+    def serve_tf_examples_fn(serialized_tf_examples):
+        feature_spec = tf_transform_output.raw_feature_spec()
+        feature_spec.pop(_LABEL_KEY)
+        parsed_features = tf.io.parse_example(serialized_tf_examples, feature_spec)
+        transformed_features = model.tft_layer(parsed_features)
+        transformed_features.pop(_transformed_name(_LABEL_KEY))
+        return model(transformed_features)
+
+    return serve_tf_examples_fn
+
+
+def _input_fn(file_pattern: Text, tf_transform_output: tft.TFTransformOutput, batch_size: int = 200) -> tf.data.Dataset:
+    transformed_feature_spec = (tf_transform_output.transformed_feature_spec().copy())
+    dataset = tf.data.experimental.make_batched_features_dataset(
+        file_pattern=file_pattern,
+        batch_size=batch_size,
+        features=transformed_feature_spec,
+        reader=_gzip_reader_fn,
+        label_key=_transformed_name(_LABEL_KEY))
+    return dataset
+
+
+def _build_keras_model() -> tf.keras.Model:
+    inputs = [keras.layers.Input(shape=(1,), name=_transformed_name(f)) for f in _FEATURE_KEYS]
+    d = keras.layers.concatenate(inputs)
+    for _ in range(3):
+        d = keras.layers.Dense(8, activation='relu')(d)
+    output = keras.layers.Dense(3, activation='softmax')(d)
+
+    model = keras.Model(inputs=inputs, outputs=output)
+    model.compile(
+        optimizer=keras.optimizers.Adam(lr=0.001),
+        loss='sparse_categorical_crossentropy',
+        metrics=[keras.metrics.SparseCategoricalAccuracy()])
+
+    model.summary(print_fn=absl.logging.info)
+    return model
+
+
+def _build_keras_model2() -> tf.keras.Model:
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(8, input_dim=4, activation='relu'))
+    model.add(keras.layers.Dense(10, activation='relu'))
+    model.add(keras.layers.Dense(8, activation='relu'))
+    model.add(keras.layers.Dense(3, activation='softmax'))
+    model.compile(
+        optimizer=keras.optimizers.Adam(lr=0.001),
+        loss='sparse_categorical_crossentropy',
+        metrics=[keras.metrics.SparseCategoricalAccuracy()])
+
+    model.summary(print_fn=absl.logging.info)
+    return model
 
 
 def preprocessing_fn(inputs):
     outputs = {}
-    
-    for key in _CATEGORICAL_FEATURE_KEYS:
-        outputs[_transformed_name(key)] = _fill_in_missing(inputs[key])
-    
-    outputs[_transformed_name(_LABEL_KEY)] = _fill_in_missing(inputs[_LABEL_KEY])
-    
+
+    for key in _FEATURE_KEYS:
+        outputs[_transformed_name(key)] = tft.scale_to_z_score(inputs[key])
+    outputs[_transformed_name(_LABEL_KEY)] = inputs[_LABEL_KEY]
+
     return outputs
 
 
-def _input_fn(filenames, tf_transform_output, batch_size):   
-    transformed_feature_spec = tf_transform_output.transformed_feature_spec().copy()
-    
-    # dataset = tf.contrib.data.make_batched_features_dataset(filenames, batch_size, transformed_feature_spec,
-    #                                                              reader=tf.data.TFRecordDataset)
-    dataset = tf.data.experimental.make_batched_features_dataset(
-        filenames, batch_size, transformed_feature_spec, reader=_gzip_reader_fn)
-    
-    transformed_features = dataset.make_one_shot_iterator().get_next()
-    
-    transformed_labels = transformed_features.pop(_transformed_name(_LABEL_KEY))
-    
-    return transformed_features, transformed_labels
+def run_fn(fn_args: TrainerFnArgs):
+    tf_transform_output = tft.TFTransformOutput(fn_args.transform_output)
 
+    train_dataset = _input_fn(fn_args.train_files, tf_transform_output, 40)
+    eval_dataset = _input_fn(fn_args.eval_files, tf_transform_output, 40)
 
-def _eval_input_receiver_fn(tf_transform_output, schema):
-    raw_feature_spec = schema_utils.schema_as_feature_spec(schema).feature_spec
-    serialized_tf_example = tf.placeholder(dtype=tf.string, shape=[None], name='input_example_tensor')
+    mirrored_strategy = tf.distribute.MirroredStrategy()
+    with mirrored_strategy.scope():
+        model = _build_keras_model()
 
-    features = tf.parse_example(serialized_tf_example, raw_feature_spec)
+    model.fit(
+        train_dataset,
+        steps_per_epoch=fn_args.train_steps,
+        validation_data=eval_dataset,
+        validation_steps=fn_args.eval_steps)
 
-    transformed_features = tf_transform_output.transform_raw_features(features)
-
-    receiver_tensors = {'examples': serialized_tf_example}
-
-    features.update(transformed_features)
-
-    return tfma.export.EvalInputReceiver(
-        features=features,
-        receiver_tensors=receiver_tensors,
-        labels=transformed_features[_transformed_name(_LABEL_KEY)])
-
-
-def _example_serving_receiver_fn(tf_transform_output, schema):
-    raw_feature_spec = schema_utils.schema_as_feature_spec(schema).feature_spec
-    raw_feature_spec.pop(_LABEL_KEY)
-    
-    raw_input_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(raw_feature_spec,
-                                                                               default_batch_size=None)
-    serving_input_receiver = raw_input_fn()
-    
-    transformed_features = tf_transform_output.transform_raw_features(serving_input_receiver.features)
-    
-    return tf.estimator.export.ServingInputReceiver(transformed_features, serving_input_receiver.receiver_tensors)
-
-
-def _build_estimator():
-    feature_columns = [
-        tf.feature_column.numeric_column(key, shape=())
-        for key in _transformed_names(_CATEGORICAL_FEATURE_KEYS)
-    ]
-    
-    classifier = tf.estimator.DNNClassifier(
-        feature_columns=feature_columns,
-        hidden_units=[10, 10],
-        n_classes=3)
-
-    return classifier
-
-
-def trainer_fn(hparams, schema):
-    train_batch_size = 32
-    eval_batch_size = 32
-    
-    tf_transform_output = tft.TFTransformOutput(hparams.transform_output)
-    
-    train_input_fn = lambda: _input_fn(
-        hparams.train_files,
-        tf_transform_output,
-        batch_size=train_batch_size)
-
-    eval_input_fn = lambda: _input_fn(
-        hparams.eval_files,
-        tf_transform_output,
-        batch_size=eval_batch_size)
-
-    train_spec = tf.estimator.TrainSpec(
-        train_input_fn,
-        max_steps=hparams.train_steps)
-
-    serving_receiver_fn = lambda: _example_serving_receiver_fn(tf_transform_output, schema)
-
-    exporter = tf.estimator.FinalExporter('iris', serving_receiver_fn)
-    eval_spec = tf.estimator.EvalSpec(
-        eval_input_fn,
-        steps=hparams.eval_steps,
-        exporters=[exporter],
-        name='iris')
-
-    estimator = _build_estimator()
-
-    receiver_fn = lambda: _eval_input_receiver_fn(tf_transform_output, schema)
-
-    return {
-        'estimator': estimator,
-        'train_spec': train_spec,
-        'eval_spec': eval_spec,
-        'eval_input_receiver_fn': receiver_fn
+    signatures = {
+        'serving_default':
+            _get_serve_tf_examples_fn(model,
+                                      tf_transform_output).get_concrete_function(
+                tf.TensorSpec(
+                    shape=[None],
+                    dtype=tf.string,
+                    name='examples')),
     }
+
+    model.save(fn_args.serving_model_dir, save_format='tf', signatures=signatures)
